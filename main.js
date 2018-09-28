8f645505098161c76636d3dc5d0b18ccc4e0877c
digi360 <sydney.sedibe@umuzi.org>
2018-09-28 12:29:01 +0200

    MyWebSite so far...

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-$(document).ready(function () {
-    $("a[href=#]").on("click", function (e) {
+$(document).ready(function ()
+{
+    $("#crime-stats").click(function(e)
+    {
         e.preventDefault();
-        //history.pushState({}, "", this.href);
-        history.replaceState({}, "", this.href);
+        let mainSec = $("main");
+        let theFrame = '<iframe src="portfolio/Crime_Stats_Group_Work.html">Your browser does not support iframes. Please upgrade and refresh to see this page. Alternatively, you can press the back button.</iframe>'
+        $("#main").html(theFrame);
+    });
+    $("#salary1").click(function(e)
+    {
+        e.preventDefault();
+        let mainSec = $("main");
+        let theFrame = '<iframe src="portfolio/Salary_Assignment1_Correlation.html">Your browser does not support iframes. Please upgrade and refresh to see this page. Alternatively, you can press the back button.</iframe>'
+        $("#main").html(theFrame);
+    });
+    $("#salary2").click(function(e)
+    {
+        e.preventDefault();
+        let mainSec = $("main");
+        let theFrame = '<iframe src="portfolio/Salary_Assignment2_Regression.html">Your browser does not support iframes. Please upgrade and refresh to see this page. Alternatively, you can press the back button.</iframe>'
+        $("#main").html(theFrame);
     });
 });
