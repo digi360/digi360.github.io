8a5e7a2075d69ab12d981c813263454e4714ebdb
digi360 <sydney.sedibe@umuzi.org>
2018-10-03 18:49:51 +0200

    Adding FindItSearchEngine to profile

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 $(document).ready(function ()
 {
+    $("#search-engine").click(function (e) {
+        e.preventDefault();
+        let mainSec = $("main");
+        let theFrame = '<iframe src="https://github.com/digi360/FindItSearchEngine/blob/master/findit.html">Your browser does not support iframes. Please upgrade and refresh to see this page. Alternatively, you can press the back button.</iframe>'
+        $("#main").html(theFrame);
+    });
     $("#crime-stats").click(function(e)
     {
         e.preventDefault();
